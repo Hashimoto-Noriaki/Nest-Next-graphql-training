6a10316d94686ad70c1dd51acffce0d53d602b58
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-11 00:11:55 +0900

    タスクモデル作成

---
+++ backend/src/task/models/task.model.ts
@@ -0,0 +1,19 @@
+import { Field, ObjectType, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Task {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  dueDate: string;
+
+  @Field()
+  status: 'NOT_STATUS' | 'PROGRESS' | 'COMPLETED';
+
+  @Field({ nullable: true })
+  description: string;
+}
