6751739b00e7a362621fdd3bbaa337034b76bce1
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-11 23:42:48 +0900

    タスク一覧を取得し、サービスのgetTasksメソッドの結果を返す

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -1,4 +1,13 @@
 import { Resolver } from '@nestjs/graphql';
+import { TaskService } from './task.service';
+import { Task } from './models/task.model';
 
 @Resolver()
-export class TaskResolver {}
+export class TaskResolver {
+  //Nest.jsが自動的にタスクサービスのインスタンスを作成し、このコンストラクターに渡してくれてタスクリゾルバーの中でタスクサービスが利用できる
+  constructor(private readonly taskService: TaskService) {}
+  //タスク一覧を取得し、サービスのgetTasksメソッドの結果を返却する
+  getTasks(): Task[] {
+    return this.taskService.getTasks();
+  }
+}
