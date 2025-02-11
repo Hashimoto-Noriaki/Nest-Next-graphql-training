da5e24e68b2337f00402a82e15e8c094c61d63e9
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-11 23:51:12 +0900

    Queryの実装

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { TaskService } from './task.service';
 import { Task } from './models/task.model';
 
@@ -7,6 +7,7 @@ export class TaskResolver {
   //Nest.jsが自動的にタスクサービスのインスタンスを作成し、このコンストラクターに渡してくれてタスクリゾルバーの中でタスクサービスが利用できる
   constructor(private readonly taskService: TaskService) {}
   //タスク一覧を取得し、サービスのgetTasksメソッドの結果を返却する
+  @Query(() => [Task], { nullable: 'items' }) //{ nullable: 'items' })は配列 [Task] の中の各要素が null になる可能性がある ことを許可する
   getTasks(): Task[] {
     return this.taskService.getTasks();
   }
