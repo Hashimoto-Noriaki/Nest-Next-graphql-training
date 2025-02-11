876292a15bfc30940648dc1e19c46a595b28dce3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-11 23:56:01 +0900

    GraphQLサーバーの起動

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -7,7 +7,7 @@ export class TaskResolver {
   //Nest.jsが自動的にタスクサービスのインスタンスを作成し、このコンストラクターに渡してくれてタスクリゾルバーの中でタスクサービスが利用できる
   constructor(private readonly taskService: TaskService) {}
   //タスク一覧を取得し、サービスのgetTasksメソッドの結果を返却する
-  @Query(() => [Task], { nullable: 'items' }) //{ nullable: 'items' })は配列 [Task] の中の各要素が null になる可能性がある ことを許可する
+  @Query(() => [Task], { nullable: 'items' }) //{ nullable: 'items' })は配列 [Task]の中の各要素がnullになる可能性があることを許可。空の配列を返却
   getTasks(): Task[] {
     return this.taskService.getTasks();
   }
