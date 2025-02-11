2ffd79cf2b2fd19dcf28885b1233580edf9fd770
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-02-11 15:21:39 +0900

    taskサービス実装

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -4,4 +4,15 @@ import { Task } from './models/task.model';
 @Injectable()
 export class TaskService {
   tasks: Task[] = [];
+
+  //タスク一覧を取得するメソッド
+  getTasks(): Task[] {
+    const task1 = new Task();
+    task1.id = 1;
+    task1.name = 'task1';
+    task1.dueDate = '2024-02-11';
+    task1.status = 'NOT_STARTED';
+    this.tasks.push(task1); //task1をタスクフィールドに追加
+    return this.tasks; //tasksを返す
+  }
 }
